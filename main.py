84b0cee5a08ff761b2cb7d27eb5db9763f008b2c
Wu, Chia-Wei <willy@loftechs.com>
2017-12-09 23:14:31 +0800

    加上Bilinear Interpolate

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ def readPhoto(name):
     return cv2.imread(name)
 
 # 儲存照片
-def savePhoto(name):
-    cv2.imwrite( name + '.png', newImage)
+def savePhoto(name, im):
+    cv2.imwrite( name + '.png', im)
 
 # 打開照片
 def showPhoto(image):
@@ -44,9 +44,38 @@ def getFunctionParaOfY():
 def getTransferCoordinate(X, Y, x, y):
     newX = X[0] * x + X[1] * y + X[2]
     newY = Y[0] * x + Y[1] * y + Y[2]
-    return(int(newX + 0.5), int(newY + 0.5)) # 四捨五入
+    return (newX, newY)
+    # return(int(newX + 0.5), int(newY + 0.5)) # 四捨五入
 
-NewPhotoFileName = 'combinePhoto'
+# 做Bilinear Interpolate
+def getBilinearInterpolate(im, x, y):
+    x = np.asarray(x)
+    y = np.asarray(y)
+
+    x0 = np.floor(x).astype(int)
+    x1 = x0 + 1
+    y0 = np.floor(y).astype(int)
+    y1 = y0 + 1
+
+    x0 = np.clip(x0, 0, im.shape[1] - 1)
+    x1 = np.clip(x1, 0, im.shape[1] - 1)
+    y0 = np.clip(y0, 0, im.shape[0] - 1)
+    y1 = np.clip(y1, 0, im.shape[0] - 1)
+
+    imA = im[y0, x0]
+    imB = im[y1, x0]
+    imC = im[y0, x1]
+    imD = im[y1, x1]
+
+    wa = (x1 - x) * (y1 - y)
+    wb = (x1 - x) * (y - y0)
+    wc = (x - x0) * (y1 - y)
+    wd = (x - x0) * (y - y0)
+
+    result = wa * imA + wb * imB + wc * imC + wd * imD
+    return result
+
+NewPhotoFileName = 'combinePhoto' # 'combineBilinearPhoto'
 
 # 讀入照片
 im1 = readPhoto("small246.jpg")
@@ -69,11 +98,11 @@ for x in range(width):
         else:
             newX, newY = getTransferCoordinate(X, Y, x, y)
             if 0 <= newY < 450 and 0 <= newX < 800:
-                # print(newX, newY, x, y)
-                newImage[y][x] = im2[newY][newX]
+                # newImage[y][x] = im2[newY][newX]
+                newImage[y][x] = getBilinearInterpolate(im2, newX, newY)
             else:
                 newImage[y][x] = 0
 print('process photo done')
 
-savePhoto(NewPhotoFileName)
+savePhoto(NewPhotoFileName, newImage)
 showPhoto(NewPhotoFileName)
